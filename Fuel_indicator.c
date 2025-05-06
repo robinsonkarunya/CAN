73963e98bbb4da884abf340ec0f4e1e7ca7ce793
Robinson P <100178732+robinsonkarunya@users.noreply.github.com>
2025-05-06 10:16:37 +0530

    Fuel indicator

---
+++ Fuel_indicator.c
@@ -0,0 +1,75 @@
+				   #include <lpc21xx.h>
+#include "header.h"
+#include "Delay.c"
+
+int adc_value = 0;
+int temp = 0;
+int percent = 0;
+
+void can_init(void) {
+    PINSEL1 |= 0x00014000;   
+    VPBDIV = 0x01;           
+    C2MOD = 0x1;             
+    C2BTR = 0x001C001D;      
+    AFMR = 0x02;             
+    C2MOD = 0x0;             
+}
+
+void adc_init(void) {
+    ADCR = 0x00200604;       
+}
+
+int ADC_Conversion() {
+    int ab;
+    ADCR |= 0x01000000;                   
+    while ((ADDR & 0x80000000) == 0);     
+    ab = (ADDR & 0x0000FFC0) >> 6;        
+    return ab;
+}
+
+void can2_tx(CAN2_MSG m1) {
+    C2TID1 = m1.id;
+    C2TFI1 = (m1.dlc << 16);
+
+    if (m1.rtr == 0) {
+        C2TFI1 &= ~(1 << 30);         
+        C2TDA1 = m1.byteA;
+        C2TDB1 = m1.byteB;
+    } else {
+        C2TFI1 |= (1 << 30);         
+    }
+
+    C2CMR = (1 << 0) | (1 << 5);     
+    while ((C2GSR & (1 << 3)) == 0);  
+	        
+    
+}
+
+int main() {
+    CAN2_MSG m1;
+  
+	 
+    can_init();
+    adc_init();
+
+    m1.rtr = 0;
+    m1.dlc = 1;
+    m1.byteB = 0;
+    m1.id = 0x03;
+
+    while (1) {
+        adc_value = ADC_Conversion();
+        percent = (adc_value * 100) / 1023;
+
+        if (percent != temp) {
+                 
+            m1.byteA = percent;
+            can2_tx(m1);
+            temp = percent;
+            Delay_milliseconds(100); 
+            
+        }
+		Delay_milliseconds(1000);
+		
+    }
+}
--- header.h
+++ header.h
@@ -1,68 +1,40 @@
-						  
-
-#ifndef __HEADER_H_
-
-#define __HEADER_H_
-
-
-#define PORT0 0
-
-#define PORT1 1
-
-#define SET   1
-
-#define CLEAR 0
-
-typedef unsigned int u32;
-
-typedef 	signed int s32;
-
-typedef unsigned char u8;
-
-typedef 	signed char s8;
-
-typedef unsigned short int u16;
-
-typedef   signed short int s16;
-
-
-typedef struct CAN2
-
-{
-
-	u32 id;
-
-	u32 rtr;
-
-	u32 dlc;
-
-	u32 byteA;
-
-	u32 byteB;
-
-}CAN2_MSG;
-
-
-
-
-
-void delay_sec(u32 sec);
-
-
-void delay_ms(unsigned int ms);
-
-
-
-
-
-void can2_init(void);
-
-void can2_tx(CAN2_MSG m1);
-
-void can2_rx(CAN2_MSG *m1);
-
-
-
-
-
-#endif
+
+#ifndef __HEADER_H_
+#define __HEADER_H_
+
+#define PORT0 0
+#define PORT1 1
+#define SET   1
+#define CLEAR 0
+typedef unsigned int u32;
+typedef 	signed int s32;
+typedef unsigned char u8;
+typedef 	signed char s8;
+typedef unsigned short int u16;
+typedef   signed short int s16;
+
+typedef struct CAN2
+{
+	u32 id;
+	u32 rtr;
+	u32 dlc;
+	u32 byteA;
+	u32 byteB;
+}CAN2_MSG;
+
+
+
+void delay_sec(u32 sec);
+
+void delay_ms(unsigned int ms);
+
+
+
+void can2_init(void);
+void can2_tx(CAN2_MSG m1);
+void can2_rx(CAN2_MSG *m1);
+
+
+
+#endif
+
